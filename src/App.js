a14c1611cacfd95530af58826c7a8b6b80e4279f
inkersal501 <inkersal501@gmail.com>
2024-11-08 22:19:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import { useState } from 'react';
+function App() {
+
+  const dictionary = [  
+      { word: "React", meaning: "A JavaScript library for building user interfaces." },  
+      { word: "Component", meaning: "A reusable building block in React." },
+      { word: "State", meaning: "An object that stores data for a component." }  
+    ];
+  const [searchInput, setSearchInput] = useState("");
+  const [showDef, setShowDef] = useState(false);
+  const [def, setDef] = useState("");
+
+  const handleForm = (e)=>{
+    e.preventDefault();
+    const filter = dictionary.filter((elem)=>{
+      return elem.word.toLowerCase().includes(searchInput.toLowerCase());
+    });
+    if(filter.length===0){
+      setDef("Word not found in the dictionary.");
+    }else{
+      setDef(filter[0].meaning);
+    }
+    setShowDef(true);
+  };
+  
+  return (
+    <div className="App">
+      <h1>Dictionary App</h1>
+      <form onSubmit={handleForm}>
+        <div>
+          <input type="text" id="searchInput" onInput={(e)=>{setSearchInput(e.target.value);setShowDef(false)}} value={searchInput} placeholder="Search for a word..."/>
+          <button type="submit">Search</button>
+        </div>
+      </form>
+      <p><b>Definition:</b></p>
+
+      {showDef && <p style={{marginTop:"20px"}}>{def}</p> }
+    </div>
+  );
+}
+
+export default App;
